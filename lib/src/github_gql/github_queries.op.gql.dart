6caa181df5718210aeda24417ed8feadb64f8d84
C1312261 Leonardo Volthier Barbosa Gomes <c1312261@interno.bb.com.br>
2021-03-22 22:58:22 -0300

    windows compatibility

---
+++ lib/src/github_gql/github_queries.op.gql.dart
@@ -0,0 +1,7 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+import 'package:gql_exec/gql_exec.dart' as _i1;
+import 'package:exemplo/src/github_gql/github_queries.ast.gql.dart' as _i2;
+
+const ViewerDetail =
+    _i1.Operation(document: _i2.document, operationName: 'ViewerDetail');
---
+++ lib/src/github_gql/github_queries.req.gql.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+import 'package:gql_exec/gql_exec.dart' as _i1;
+import 'package:exemplo/src/github_gql/github_queries.var.gql.dart' as _i2;
+import 'package:exemplo/src/github_gql/github_queries.op.gql.dart' as _i3;
+
+class ViewerDetail extends _i1.Request {
+  ViewerDetail(
+      _i2.ViewerDetailVarBuilder Function(_i2.ViewerDetailVarBuilder) buildVars)
+      : super(
+            operation: _i3.ViewerDetail,
+            variables: buildVars(_i2.ViewerDetailVarBuilder()).variables);
+}
